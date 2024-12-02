5539a4bbb34efc1409302c5a676d300ebf3900bb
Elena Marsili <e.marsili89@gmail.com>
2024-12-02 09:26:21 +0100

    Side effect cleanup

--- L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
+++ L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
@@ -4,7 +4,17 @@ function ChildComponent() {
   const [color, setColor] = useState("red");
 
   useEffect(() => {
-    setTimeout(() => setColor("green"), 3000);
+    let unmounted = false;
+
+    setTimeout(() => {
+      if (!unmounted) {
+        setColor("green");
+      }
+    }, 3000);
+
+    return () => {
+      unmounted = true;
+    };
   });
 
   return <p style={{ color }}>{color}</p>;
