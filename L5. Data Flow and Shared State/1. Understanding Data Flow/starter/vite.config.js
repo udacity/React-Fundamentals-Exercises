d92f6cb88a77806c71470a8dc2c727551c4a04c2
udacity <udacity@udacity.com>
2025-12-28 00:00:01 +0000

    Update styles, dependencies, and Vite configurations.

--- L5. Data Flow and Shared State/1. Understanding Data Flow/starter/vite.config.js
+++ L5. Data Flow and Shared State/1. Understanding Data Flow/starter/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  base: "./",
+
+  preview: {
+    host: "0.0.0.0",
+    port: 3001,
+    strictPort: true,
+
+    allowedHosts: ["localhost", ".prod.udacity-student-workspaces.com"],
+  },
+});
